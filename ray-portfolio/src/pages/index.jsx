98d9a2fae133aa0c41fc4c7355b35ae7b0bcc091
1998rtran <1998rtran@gmail.com>
2023-08-30 14:20:52 -0700

    created function to return to top for navbar, adding technology images

--- ray-portfolio/src/pages/index.jsx
+++ ray-portfolio/src/pages/index.jsx
@@ -1,8 +1,9 @@
-import Navbar from '@/components/Navbar'
-import AboutMe from '@/components/AboutMe'
-import Contact from '@/components/Contact'
-import TechSkills from '@/components/TechSkills'
-import Projects from '@/components/Projects'
+import Navbar from '@/components/Navbar';
+import AboutMe from '@/components/AboutMe';
+import Contact from '@/components/Contact';
+import TechSkills from '@/components/TechSkills';
+import Projects from '@/components/Projects';
+import Technologies from '@/components/Technologies';
 
 export default function index() {
   return (
@@ -11,6 +12,7 @@ export default function index() {
       <div className="mainpage">
         <AboutMe />
         <Projects />
+        <Technologies />
         <TechSkills />
         <Contact />
       </div>
