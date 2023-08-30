e68797ce45f9c133aad928989837ea6eebd9a1ea
1998rtran <1998rtran@gmail.com>
2023-08-30 15:26:04 -0700

    added button to share resume

--- ray-portfolio/src/components/AboutMe.jsx
+++ ray-portfolio/src/components/AboutMe.jsx
@@ -3,6 +3,7 @@ export default function AboutMe() {
     <div>
       <img className="profilephoto" alt="Nala and me" src="/profilephoto.jpg"/>
       <h2>Hello, my name is Raymond. I am a Full Stack Software Engineer</h2>
+      <a className="resumelink" target='_blank' href="Raymond Tran Resume.pdf"><div className="resumebutton">Resume</div></a>
     </div>
   )
 }
