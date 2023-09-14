f05b2b1e9324ccbd644fcb532d1495774b225037
1998rtran <1998rtran@gmail.com>
2023-09-13 23:06:39 -0700

    added current technologies list

--- ray-portfolio/src/components/Technologies.jsx
+++ ray-portfolio/src/components/Technologies.jsx
@@ -1,10 +1,13 @@
-
+import { TbBrandNextjs } from 'react-icons/tb'
+import { FaReact, FaHtml5, FaCss3Alt } from 'react-icons/fa'
 export default function Technologies() {
   return (
     <div>
-      Testing
-      <img src="/react.svg"/>
-      <img src="/next.svg"/>
+      <h3>Technologies</h3>
+      <FaReact className="reactjsicon" size={50} />
+      <FaHtml5 className="htmlicon" size={50} />
+      <FaCss3Alt className="cssicon" size={50} />
+      <TbBrandNextjs className="nextjsicon" size={50} />
     </div>
   )
 }
