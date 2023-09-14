abe14b9de030ed48f96931c826e6c69d4f75291b
1998rtran <1998rtran@gmail.com>
2023-09-13 23:58:47 -0700

    added github links and fixed sizing for icons

--- ray-portfolio/src/components/Projects.jsx
+++ ray-portfolio/src/components/Projects.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Projects() {
 
@@ -25,6 +26,7 @@ export default function Projects() {
             <p> - Achieved Google Lighthouse scores of 49, 100, 92, and 92 for performance, accessibility, best practices, and SEO respectively</p>
             <p> - Implemented Firebase Authentication to check registered users for accessibility</p>
             <p> - Managed and stored user login data in Firestore Database while securing and hiding user information</p>
+            <a target="_blank" className="githublink" href=""><FaGithubSquare /></a>
           </div>
         </div>
         <div id="projectcard" className="projectcard">
@@ -36,6 +38,7 @@ export default function Projects() {
             <p> - Built and styled a fully functional web application with CRUD capabilities using React.js and raw CSS</p>
             <p> - Utilized MongoDB, Cloudinary API, and Express.js to query and store data efficiently</p>
             <p> - Configured Auth0 to implement a sign-in feature for authenticated users to give access for full web functionalities</p>
+            <a target="_blank" className="githublink" href=""><FaGithubSquare /></a>
           </div>
         </div>
         <div id="projectcard" className="projectcard">
@@ -47,6 +50,7 @@ export default function Projects() {
             <p> - Implemented lazy loading for images and content to optimize Largest Contentful Paint by 3 seconds</p>
             <p> - Optimized latency by configuring NGINX Load Balancer and Caching, reducing CPU bottleneck from 90% to 16%, increasing successful client requests per second from 1000 to 3500, improving average response times from 300ms to 62ms, while maintaining 0% error rate.</p>
             <p> - Deployed the entire application to Amazon AWS (EC2) and utilized Imaging from AWS to mimic identical instances for accurate reproduction of a server</p>
+            <a target="_blank" className="githublink" href=""><FaGithubSquare /></a>
           </div>
         </div>
       </div>
