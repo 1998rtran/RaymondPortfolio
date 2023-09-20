067ad4ffb41b4740c30e88fa410f37909bbbcbb0
1998rtran <1998rtran@gmail.com>
2023-09-19 23:08:03 -0700

    adjusted scroll feature for project title to under navbar

--- ray-portfolio/src/components/Projects.jsx
+++ ray-portfolio/src/components/Projects.jsx
@@ -10,7 +10,9 @@ export default function Projects() {
     const clickHandler = (event) => {
       const projectcard = event.currentTarget.parentElement;
       projectcard.classList.toggle('active');
-      projectcard.scrollIntoView({ behavior: 'smooth' });
+      // projectcard.scrollIntoView({ behavior: 'smooth' });
+      const scrollPosition = projectcard.offsetTop - 38;
+      window.scrollTo({ top: scrollPosition, behavior: 'smooth' });
     };
 
     for (let i = 0; i < projtitles.length; i++) {
