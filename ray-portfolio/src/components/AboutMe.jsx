b035f1ecf7fbddbe6e5540e1ee52b4efa2cfe821
1998rtran <1998rtran@gmail.com>
2023-09-29 15:13:07 -0700

    changing colorway and stylings

--- ray-portfolio/src/components/AboutMe.jsx
+++ ray-portfolio/src/components/AboutMe.jsx
@@ -5,13 +5,14 @@ export default function AboutMe() {
         <img className="profilephoto" alt="Nala and me" src="/profilephoto.jpg"/>
       {/* </div> */}
       <div className="intro">
-      <h2>Hello, my name is Raymond.</h2>
+      <h2>Hello, my name is Raymond</h2>
       <h2>I am a Full Stack Software Engineer</h2>
       <h3>Click here to learn about me!</h3>
       </div>
         <a className="resumelink" target='_blank' href="Raymond Tran Resume.pdf">
           <div className="resumebutton">Resume</div>
         </a>
+        <p>Cabin Condensed</p>
       </div>
   )
 }
